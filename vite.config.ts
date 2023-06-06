1d4af63cd3cf7c94b16bef8ad8a8a6b0a3f7bce9
dcyao <1207542617@qq.com>
2023-06-06 11:38:53 +0800

    去除eslint 和增加了一些配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import eslintPlugin from 'vite-plugin-eslint';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    eslintPlugin({
-			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
-		}),
+    vue(),
+    VueSetupExtend(),
     AutoImport({
     resolvers: [ElementPlusResolver()],
   }),
   Components({
     resolvers: [ElementPlusResolver()],
-  }),vue(),],
+  }),],
 
   resolver: {
     alias: {
